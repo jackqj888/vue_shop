e6b947e8e15996184b89bc0e573a15269e40c68f
qjgod <11325176@qq.com>
2020-11-24 16:42:57 +0800

    登入页面测试和跳转

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import { Button, Form, FormItem, Input, message } from 'element-ui'
+// 导入弹窗提示组件
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.prototype.$message = message
