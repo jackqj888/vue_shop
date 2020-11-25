17a49e4040361f68910fdf352480847ba7acc04a
qjgod <11325176@qq.com>
2020-11-25 16:44:47 +0800

    后台管理侧边栏循环完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,101 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <el-header>
+        <!-- 头部区域 -->
+      <div>
+         <img src="../assets/heima.png" alt="">
+         <span>电商后台管理系统</span>
+      </div>
+    <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+        <!-- 侧边栏 -->
+      <el-aside width="200px">
+          <!-- 侧边栏菜单区域 -->
+        <el-menu
+      background-color="#333744"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <!-- 一级菜单 -->
+      <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+          <!-- 一级菜单的模板区域 -->
+        <template slot="title">
+            <!-- i 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- span 文本 -->
+          <span>{{item.authName}}</span>
+        </template>
+        <!-- 二级菜单 -->
+          <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id"><template slot="title">
+            <!-- i 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- span 文本 -->
+          <span>{{subItem.authName}}</span>
+        </template></el-menu-item>
+      </el-submenu>
+    </el-menu>
+      </el-aside>
+       <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      menulist: []
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear() // 清空 token
       this.$router.push('/login') // 跳转 login
+    },
+    // 获取所有的菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
     }
   }
-
 }
 </script>
 
 <style lang="less" scoped>
 
+.home-container {
+
+    height: 100%;
+}
+.el-header {
+    background-color: #373d41;
+    display:flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
+}
+
+.el-aside {
+    background-color: #333744;
+}
+
+.el-main {
+    background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, message } from 'element-ui'
+import { Button, Form, FormItem, Input, message, Container, Header, Aside, main, menu, submenu, MenuItem } from 'element-ui'
 // 导入弹窗提示组件
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(menu)
+Vue.use(main)
+Vue.use(submenu)
+Vue.use(MenuItem)
 Vue.prototype.$message = message
