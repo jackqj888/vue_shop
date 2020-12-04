f947f70288acbffd7892467c40a9a5a9620ae14c
qjgod <11325176@qq.com>
2020-12-04 16:35:58 +0800

    权限列表获取

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
+import Rights from './components/power/Rights.vue'
 
 Vue.use(Router)
 
@@ -16,7 +17,9 @@ const router = new Router({
       component: Home,
       redirect: '/Welcome',
       children: [
-        { path: '/Welcome', component: Welcome }, { path: '/Users', component: Users }
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights }
       ]
     }
   ]
