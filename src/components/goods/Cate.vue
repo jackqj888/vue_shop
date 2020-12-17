0ef40c6e506a574b1a627a470d46f4175336d04d
qjgod <11325176@qq.com>
2020-12-17 16:50:14 +0800

    商品分类的建立

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+         <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <el-row>
+            <el-col>
+                <el-button type="primary">添加分类</el-button>
+            </el-col>
+        </el-row>
+        <!-- 表格 -->
+
+        <!-- 分页区域 -->
+    </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表,默认为空
+      catelist: [],
+      total: 0
+    }
+  },
+  //   生命周期函数
+  created () {
+    this.getCateList()
+  },
+  //   事件处理函数
+  methods: {
+    //   获取商品分类数据
+    async getCateList () {
+      const { tada: res } = await this.$http.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) { return this.$message.error('获取商品分类失败！') }
+      //   把数据列表赋值给 catelist
+      this.catelist = res.data.result
+      //   为总数据条数赋值
+      this.total = res.data.total
+    }
+  }
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
