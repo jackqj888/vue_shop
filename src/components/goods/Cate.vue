b42ddb200bb9857ef09a0e2adec681e4b52ae177
qjgod <11325176@qq.com>
2020-12-18 16:54:32 +0800

    分类名称显示

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -15,6 +15,7 @@
             </el-col>
         </el-row>
         <!-- 表格 -->
+        <tree-table :data="catelist" :columns="columns" :selection-type="false" :expand-type="false" show-index></tree-table>
 
         <!-- 分页区域 -->
     </el-card>
@@ -33,7 +34,14 @@ export default {
       },
       // 商品分类的数据列表,默认为空
       catelist: [],
-      total: 0
+      // 总数据条数
+      total: 0,
+      // 为table指定列的定义
+      columns: [{
+        label: '分类名称',
+        prop: 'cat_name'
+      }]
+
     }
   },
   //   生命周期函数
@@ -44,7 +52,7 @@ export default {
   methods: {
     //   获取商品分类数据
     async getCateList () {
-      const { tada: res } = await this.$http.get('categories', { params: this.queryInfo })
+      const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
       if (res.meta.status !== 200) { return this.$message.error('获取商品分类失败！') }
       //   把数据列表赋值给 catelist
       this.catelist = res.data.result
